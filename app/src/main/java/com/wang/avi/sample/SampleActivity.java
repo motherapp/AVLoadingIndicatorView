7d8058356ae0052e6ccdf86a607c35b5b31f87dc
Kevin <kevin@motherapp.com>
2021-09-27 13:39:43 +0800

    feat: migrate to androidx

--- app/src/main/java/com/wang/avi/sample/SampleActivity.java
+++ app/src/main/java/com/wang/avi/sample/SampleActivity.java
@@ -2,10 +2,10 @@ package com.wang.avi.sample;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 
